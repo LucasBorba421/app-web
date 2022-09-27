5cd9d56c91667f8b1be0da8a0970128854d49092
LucasBorba421 <97745079+LucasBorba421@users.noreply.github.com>
2022-09-27 13:37:40 -0300

    loading

--- src/app/pages/details/details.component.ts
+++ src/app/pages/details/details.component.ts
@@ -13,12 +13,16 @@ export class DetailsComponent implements OnInit {
   constructor( private route: ActivatedRoute ) {}
 
   public id: number = this.route.snapshot.params.id;
+  public loading: boolean = true;
   public recommendation!: RecommendationModel;
 
   public ngOnInit(): void {
-    this.recommendation = dataset.filter((item) => {
-      return item.id == this.id;
-    })[0];
+    setTimeout(() => {
+      this.recommendation = dataset.filter((item) => {
+        return item.id == this.id;
+      })[0];
 
+      this.loading = false;
+    }, 500);
   }
 }
