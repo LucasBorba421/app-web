e22e1e3e17098d537acce45f79900f03316a011c
LucasBorba421 <97745079+LucasBorba421@users.noreply.github.com>
2022-09-27 13:01:04 -0300

    a base do feed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { HomeComponent } from './pages/home/home.component';
+import { RecommendationItemComponent } from './components/recommendation-item/recommendation-item.component';
+
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, 
+    HomeComponent, 
+    RecommendationItemComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/data/recommendations.ts
@@ -0,0 +1,44 @@
+export const dataset = [
+  {
+    id: 1,
+    title: 'Breaking Bad',
+    kind: 'Série',
+    description: 'Melhor série já criada na vida',
+  },
+  {
+    id: 2,
+    title: 'Peacemaker',
+    kind: 'Série',
+    description: 'Muita loucura, ação, humor e tem a melhor abertura das séries.',
+  },
+  {
+    id: 3,
+    title: 'O Hobbit',
+    kind: 'Livro',
+    description: 'Uma aventura leve e envolvente, que serve de porta de entrada para o Universo criado por Tolkien',
+  },
+  {
+    id: 4,
+    title: '1984',
+    kind: 'Livro',
+    description: 'Uma distopia obrigatória! Descubra de onde se originou o nome Big Brother.',
+  },
+  {
+    id: 5,
+    title: 'Berserk',
+    kind: 'Manga',
+    description: 'Fantasia medielval, com uma pegada mais adulta e pessimista. Uma história cheia de reviravoltas e muito sangue.',
+  },
+  {
+    id: 6,
+    title: 'Hollow Knight',
+    kind: 'Jogo',
+    description: 'Jogo bonito para passar raiva',
+  },
+  {
+    id: 7,
+    title: 'Dark Souls',
+    kind: 'Jogo',
+    description: 'Essa saga de jogos chama a atenção por sua dificuldade amarga, mas isso é só a superfice. Jogue qualquer titulo dessa saga e se apaixone por sua história vasta, cheia de aventura e morte.',
+  },
+]
