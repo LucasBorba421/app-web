fffb0d79d06fe73d9e004696584e9e5554a80e92
LucasBorba421 <97745079+LucasBorba421@users.noreply.github.com>
2022-09-27 13:01:54 -0300

    OnInit e Filter

--- src/app/pages/details/details.component.ts
+++ src/app/pages/details/details.component.ts
@@ -2,12 +2,23 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { RecommendationModel } from './../../models/recommendation.model';
 
+import { dataset } from '../../data/recommendations';
+
 
 @Component({
   selector: 'app-details',
   templateUrl: './details.component.html',
 })
-export class DetailsComponent {
+export class DetailsComponent implements OnInit {
   constructor( private route: ActivatedRoute ) {}
 
+  public id: number = this.route.snapshot.params.id;
+  public recommendation!: RecommendationModel;
+
+  public ngOnInit(): void {
+    this.recommendation = dataset.filter((item) => {
+      return item.id == this.id;
+    })[0];
+
   }
+}
