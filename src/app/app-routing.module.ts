5e4cc7a9bb9ac987780980d91cc8cff1f0c25694
LucasBorba421 <97745079+LucasBorba421@users.noreply.github.com>
2022-09-29 14:31:14 -0300

    2 files modified, 3 added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,8 +3,12 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { HomeComponent } from './pages/home/home.component';
 import { DetailsComponent } from './pages/details/details.component';
+import { LoginComponent } from './pages/login/login.component';
+
+import { AuthGuard } from './guards/auth.guard';
 
 const routes: Routes = [
+  { path: 'login', component: LoginComponent },
   { path: '', component: HomeComponent },
   { path:':id/detalhes', component: DetailsComponent},
 ];
