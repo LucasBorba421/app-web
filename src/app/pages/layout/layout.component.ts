891bd62d80434b46491f4c0ddc36e10ef12582ff
LucasBorba421 <97745079+LucasBorba421@users.noreply.github.com>
2022-09-29 14:48:11 -0300

    layout

--- src/app/pages/layout/layout.component.ts
+++ src/app/pages/layout/layout.component.ts
@@ -3,7 +3,8 @@ import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-layout',
-  templateUrl: './layout.component.html'
+  templateUrl: './layout.component.html',
+  styleUrls: ['./layout.component.css']
 })
 export class LayoutComponent {
   constructor(private router: Router) {}
