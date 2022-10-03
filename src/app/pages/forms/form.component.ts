21ce6d88f974a87bc954d05412d27043dad341b0
LucasBorba421 <97745079+LucasBorba421@users.noreply.github.com>
2022-10-03 13:16:11 -0300

    1 file modified

--- src/app/pages/forms/form.component.ts
+++ src/app/pages/forms/form.component.ts
@@ -13,9 +13,7 @@ import { Router } from '@angular/router';
 export class FormComponent implements OnInit{
  constructor( private httpClient: HttpClient, private router: Router){}
 
-  public categories: CategoryModel[] = []
-
-
+  public categories: CategoryModel[] = [];
   public form: FormGroup = new FormGroup({
     title: new FormControl('', [Validators.required]),
     kind: new FormControl('', [Validators.required]),
@@ -29,11 +27,23 @@ export class FormComponent implements OnInit{
   }
 
   public save(): void {
+    const url = 'https://jp-recommendations-api.herokuapp.com/recommendation';
 
     if (this.form.valid) {
-      console.log(this.form.value)
+     this.httpClient.post(url, this.form.value).toPromise().then((_) => {
+       this.router.navigateByUrl('');
+     }).catch((response) => {
+       alert(response.error.error);
+     });
     } else {
       alert('Formulário Inválido')
     }
+  }
+
+  private loadCategories(): void {
+    const url = 'https://jp-recommendations-api.herokuapp.com/categories';
+    this.httpClient.get<CategoryModel[]>(url).toPromise().then((data) => {
+        this.categories = data;
+      });
   }
 }
