0970a7fef0a0b62f976be24cadb9aaf76ea7c070
LucasBorba421 <97745079+LucasBorba421@users.noreply.github.com>
2022-10-12 16:52:46 -0300

    1 file modified

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -3,7 +3,6 @@ import { Component, OnInit } from '@angular/core';
 import { RecommendationModel } from '../../models/recommendation.model';
 import { CategoryModel } from '../../models/category.model';
 import { AuthService } from '../../services/auth.service';
-import { dataset } from '../../data/recommendations';
 import { HttpClient } from '@angular/common/http';
 
 @Component({
@@ -17,36 +16,37 @@ export class HomeComponent implements OnInit {
     private httpClient: HttpClient
   ) {}
 
+  public readonly ALL_RECOMMENDATIONS: number = 0;
   public recommendations?: RecommendationModel[] = [];
-  public kinds?: CategoryModel[] = [];
-  public currentKind: string = 'all';
+  public categories?: CategoryModel[] = [];
+  public currentCategory: number = this.ALL_RECOMMENDATIONS;
   public currentUser: string = this.authService.currentUser;
+  public loading: boolean = true;
 
   ngOnInit(): void {
     this.loadCategories();
-    this.loadRecommendations();
+    this.loadRecommendations(this.ALL_RECOMMENDATIONS);
   }
 
-  public filter(kind: CategoryModel): void {
-    this.currentKind = kind.id;
-    if (kind.name == 'all') {
-      this.recommendations = dataset;
-    } else {
-      this.recommendations = dataset.filter((item) => item.kind == kind);
-    }
+  public filter(categoryId: number): void {
+    this.currentCategory = categoryId;
+    this.loadRecommendations(categoryId)
   }
 
-  private loadRecommendations(): void {
+  private loadRecommendations(categoryId: number): void {
     const url = 'https://jp-recommendations-api.herokuapp.com/recommendations';
-    this.httpClient.get<RecommendationModel[]>(url).toPromise().then((data) => {
+    const params: any = categoryId != this.ALL_RECOMMENDATIONS ? { category: categoryId } : {}
+    this.loading = true;
+    this.httpClient.get<RecommendationModel[]>(url, ( params )).toPromise().then((data) => {
       this.recommendations = data;
+      this.loading = false;
       });
   }
 
   private loadCategories(): void {
     const url = 'https://jp-recommendations-api.herokuapp.com/categories';
     this.httpClient.get<CategoryModel[]>(url).toPromise().then((data) => {
-      this.kinds = data;
+      this.categories = data;
       });
   }
 }
